e7d6a6f130815ec6cedff06d7234d1631accba62
pankajsalunke <pankajsalunke305@gmail.com>
2025-07-02 19:00:34 +0530

    app update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 // app.use(express.
 app.use(cookieParser());
-app.use(express.static(path.resolve("client")));
+app.use(express.static(path.resolve("public")));
 // app.use(express.static("public"));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve("dist", "index.html"));
