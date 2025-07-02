c23b2c877f123194afa155c436b4b7b09ac48d0a
pankajsalunke <pankajsalunke305@gmail.com>
2025-07-02 19:17:04 +0530

    update

--- app.js
+++ app.js
@@ -43,6 +43,15 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // API Routes
 import RegisterRouter from "./src/routes/register.routes.js";
 import taskrouter from "./src/routes/task.routes.js";
