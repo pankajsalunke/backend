1f695b928e268c87607f8abb8cb4cd618fb65e35
pankajsalunke <pankajsalunke305@gmail.com>
2025-07-02 19:21:33 +0530

    app update

--- app.js
+++ app.js
@@ -47,9 +47,9 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 // API Routes
